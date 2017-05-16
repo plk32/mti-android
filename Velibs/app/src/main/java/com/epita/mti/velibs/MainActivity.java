e348cdaaea3dc059767bf08002182192e9348dbc
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-05-17 01:05:21 +0200

    Search is ok

--- Velibs/app/src/main/java/com/epita/mti/velibs/MainActivity.java
+++ Velibs/app/src/main/java/com/epita/mti/velibs/MainActivity.java
@@ -1,13 +1,23 @@
 package com.epita.mti.velibs;
 
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,12 +36,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected()) {
-
-            FetchData();
+            FetchData(null);
 
             mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
             mRecyclerView.setHasFixedSize(true);
@@ -42,7 +56,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void FetchData() {
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = getIntent().getStringExtra(SearchManager.QUERY).toUpperCase();
+            FetchData(query);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_items_main_activity, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_credit:
+                Toast.makeText(MainActivity.this, "Credits to :\nOufkir Moussa\nSaint-Genez Lorris", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void FetchData(final String query) {
         final SingletonVelib singletonVelib = SingletonVelib.getInstance();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(OpenDataService.ENDPOINT)
@@ -55,8 +100,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<VelibResponse> call, Response<VelibResponse> response) {
                 if (response.isSuccessful()) {
-                    Log.v("OnRESPONSE", response.body().getRecords().get(0).getFields().getLastUpdate());
-                    singletonVelib.setVelibs(response.body().getRecords());
+                    if (query != null) {
+                        ArrayList<Velib> compatibleVelibAdresses = new ArrayList<>();
+                        for (Velib v : response.body().getRecords()) {
+                            if (v.getFields().getAddress().contains(query))
+                                compatibleVelibAdresses.add(v);
+                        }
+                        if (compatibleVelibAdresses.size() > 0)
+                            singletonVelib.setVelibs(compatibleVelibAdresses);
+                        else {
+                            Toast.makeText(MainActivity.this, "No result found", Toast.LENGTH_SHORT).show();
+                            singletonVelib.setVelibs(response.body().getRecords());
+                        }
+                    }
+                    else
+                        singletonVelib.setVelibs(response.body().getRecords());
                     mAdapter.setData(singletonVelib.getVelibs());
                     mAdapter.notifyDataSetChanged();
                 }
--- Velibs/app/src/main/java/com/epita/mti/velibs/PagerActivity.java
+++ Velibs/app/src/main/java/com/epita/mti/velibs/PagerActivity.java
@@ -1,8 +1,13 @@
 package com.epita.mti.velibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by Lorris on 12/05/2017.
@@ -17,11 +22,39 @@ public class PagerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_pager);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_view_pager);
+        setSupportActionBar(toolbar);
+
         SingletonVelib singletonVelib = SingletonVelib.getInstance();
 
         mSPA = new SectionsPagerAdapter(getSupportFragmentManager(), singletonVelib.getVelibs());
         mVP = (ViewPager) findViewById(R.id.container);
         mVP.setAdapter(mSPA);
         mVP.setCurrentItem((int) getIntent().getExtras().get("position"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_items_view_pager, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_credit:
+                Toast.makeText(PagerActivity.this, "Credits to :\nOufkir Moussa\nSaint-Genez Lorris", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.first_action:
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Check out this app for velibs free spots !");
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.share)));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
